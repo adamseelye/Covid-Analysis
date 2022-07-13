02956ef56974fc6773098ee13bc48c9b4e9e28f3
Roman Davis <fluffyribbit@gmail.com>
2022-07-13 16:15:51 -0400

    Added better debugging for test views; added bootstrap; confirmed country bug

--- app/controllers/Test.scala
+++ app/controllers/Test.scala
@@ -10,8 +10,15 @@ import models.DaySeries
 
 class Test @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def country(country: String, stat: String)= Action { implicit request =>
+    // TODO: It looks like we're getting multiple inputs for the same date for
+    // because of the value 
     val dayseries = models.Day.dayseries.country(country).by_date()
-   Ok(views.html.test.country(dayseries, country, stat))
+    Ok(views.html.test.country(dayseries, country, stat))
+  }
+
+  def state(state: String, stat: String) = Action { implicit request =>
+    val dayseries = models.Day.dayseries.state(state).by_date()
+    Ok(views.html.test.state(dayseries, state, stat))
   }
 
   // def state(string: State, stat: String) = Action { implicit request =>
