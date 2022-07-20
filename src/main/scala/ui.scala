3f8b75267b7168cb835841fa8dbfd329857bc6dd
SevinHawkins <107058263+SevinHawkins@users.noreply.github.com>
2022-07-20 11:18:12 -0500

    Update ui.scala
    Deleted greeting placeholder

--- src/main/scala/ui.scala
+++ src/main/scala/ui.scala
@@ -1,12 +1,4 @@
 object ui {
-  def greeting(): Unit = {
-    // this will either be the main ui or the CLI interface
-    // with the main ui on the web
-    println("COVID ANALYSIS")
-    println("")
-    println("Please visit <hostname:port> in your web browser")
-  }
-
   // Command Line interfsce
   // Use in case web interface is not feasible
 
