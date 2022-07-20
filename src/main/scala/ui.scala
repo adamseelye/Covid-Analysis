9f0c76aa9754256217e19c8a16012e9d5c3a5696
SevinHawkins <107058263+SevinHawkins@users.noreply.github.com>
2022-07-20 11:17:06 -0500

    Update ui.scala
    Created a basic template for a CLI

--- src/main/scala/ui.scala
+++ src/main/scala/ui.scala
@@ -6,4 +6,39 @@ object ui {
     println("")
     println("Please visit <hostname:port> in your web browser")
   }
+
+  // Command Line interfsce
+  // Use in case web interface is not feasible
+
+  // main function that will start the application
+  def main (args: Array[String]): Unit = {
+    sparkQueries.cxn()
+    mainMenu()
+  }
+
+  // main menu
+  def mainMenu(): Unit = {
+    // add menu options where appropriat
+
+    val input = scala.io.StdIn.readLine()
+    input match {
+      // make case statement based on input
+      // statement to call queries function
+      // will set up a case statement when or if that time comes
+
+    queriesMenu()
+
+    }
+
+  }
+
+  def queriesMenu(): Unit = {
+    println("Please select a query")
+    // add menu options where appropriate
+
+    val input = scala.io.StdIn.readLine()
+    input match {
+      // create case statements for this menu
+    }
+  }
 }
