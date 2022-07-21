7eba2ed67c6a15f1dbbf52259dcbb6d9a14ef349
Roman Davis <fluffyribbit@gmail.com>
2022-07-21 02:50:47 -0400

    Added sort links to the top of the country page

--- app/controllers/Country.scala
+++ app/controllers/Country.scala
@@ -11,11 +11,11 @@ import models.DaySeries
 class Country @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def view(column: String = "Country", order: String = "ASC") = Action { implicit request =>
     val countries = models.Day.dayseries.country_sums(column, order)
-    Ok(views.html.country.view(countries))
+    Ok(views.html.country.view(countries, column, order))
   }
 
   def states(country: String = "ALL", column: String = "State", order: String = "ASC") = Action { implicit request =>
     val states = models.Day.dayseries.state_sums(country, column, order)
-    Ok(views.html.country.states(states, country))
+    Ok(views.html.country.states(states, country, column, order))
   }
 }
